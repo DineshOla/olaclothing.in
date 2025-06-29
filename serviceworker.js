68c557c111a52da0c0290f3b858c5a58eb9430e1
Dinesh Kumar <ola.dinesh99@gmail.com>
2025-06-30 00:46:43 +0530

    Initial commit

--- serviceworker.js
+++ serviceworker.js
@@ -1,59 +1,22 @@
-const CACHE_NAME = 'ola-clothing-cache-v1';
-const urlsToCache = [
-    '/',
-    '/index.html',
-    '/styles.css',
-    '/scriptaddtcart.js',
-    '/img/Main-Banners.jpg',
-    '/img/Main-Banners-mobile.jpg',
-    '/img/Main-Banners-tablet.jpg',
-    '/img/men_shirts.jpeg',
-    '/img/men_denim.jpg.webp',
-    '/img/men_polos&tees.jpg',
-    '/img/men_shoes.jpg',
-    '/img/women_bags.jpg',
-    '/img/women_dresses.jpg',
-    '/img/women_tops.jpg',
-    '/img/women_jeans.jpg',
-    '/img/icon-192x192.png',
-    '/img/icon-512x512.png'
-];
-
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                console.log('Opened cache');
-                return cache.addAll(urlsToCache);
-            })
-            .catch(error => {
-                console.error('Cache addAll failed:', error);
-            })
+        caches.open('v1').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/styles.css',
+                '/scriptaddtcart.js',
+                '/img/icon-192x192.png',
+                '/img/icon-512x512.png'
+            ]).catch(err => console.error('Cache addAll failed. Missing files:', err));
+        })
     );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                return response || fetch(event.request).catch(() => {
-                    console.error('Fetch failed for:', event.request.url);
-                });
-            })
-    );
-});
-
-self.addEventListener('activate', event => {
-    const cacheWhitelist = [CACHE_NAME];
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if (!cacheWhitelist.includes(cacheName)) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request).catch(err => console.log('Fetch failed for:', event.request.url, err));
         })
     );
 });
